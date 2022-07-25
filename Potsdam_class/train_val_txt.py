e6f7ce973b74a12843676bc4e0cf2217d7b980f7
DaYinYi <luohuiyin0524@163.com>
2022-07-25 15:19:07 +0800

    "将训练集和验证集的图片进行读取，得到txt文件"

---
+++ Potsdam_class/train_val_txt.py
@@ -0,0 +1,25 @@
+# encoding: utf-8
+import os
+
+
+def to_text(src, dst1):
+    txt = []
+    filenames = os.listdir(src)
+    for item in filenames:
+        name = item.split('.')[0]
+        txt.append(name)
+
+    fo = open(dst1, 'w')
+    for m in txt:
+        fo.write(str(m) + '\n')
+
+
+if __name__ == "__main__":
+    train_path = r'H:\\datasets\\Vovpotsdam\\Train\\image'
+    val_path = r'H:\\datasets\\Vovpotsdam\\Val\\image'
+
+    dst_train = r'H:\\datasets\\Vovpotsdam\\ImageSets\\Segmentation\\train.txt'
+    dst_val = r'H:\\datasets\\Vovpotsdam\\ImageSets\\Segmentation\\val.txt'
+
+    to_text(train_path, dst_train)
+    to_text(val_path, dst_val)
